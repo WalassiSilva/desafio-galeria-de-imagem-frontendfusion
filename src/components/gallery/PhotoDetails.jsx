c6ed1f7cb4115d6ca48344abb6875c4ac9d3004a
Walassi Silva <walassi.b@gmail.com>
2024-10-19 14:42:47 -0300

    style: change photo details style

--- src/components/gallery/PhotoDetails.jsx
+++ src/components/gallery/PhotoDetails.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+
 export default function PhotoDetails({ selectedImage, setSelectedImage }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div
       onClick={() => setSelectedImage(null)}
@@ -8,15 +17,22 @@ export default function PhotoDetails({ selectedImage, setSelectedImage }) {
         className="bg-white p-8 rounded-lg flex flex-col relative"
         onClick={(e) => e.stopPropagation()}
       >
-        <span>LIKE❤</span>
+        <span className="absolute top-10 right-10 z-50 cursor-pointer" onClick={handleLike}>
+          <FaHeart
+            size={40}
+            className={`${
+              isLiked ? "text-red-600" : "text-white"
+            } transition duration-300 ease-in-out hover:scale-110`}
+          />
+        </span>
         <img
           src={selectedImage?.download_url}
           alt={selectedImage?.author}
           className="rounded-lg object-contain h-96"
         />
         <div>
-          <h2>Author: {selectedImage?.author}</h2>
-          <h4>
+          <h3 className="text-lg font-bold text-zinc-900">Author: {selectedImage?.author}</h3>
+          <h4 className="font-semibold text-zinc-600">
             Dimensions: {selectedImage?.width}x{selectedImage?.height}
           </h4>
         </div>
