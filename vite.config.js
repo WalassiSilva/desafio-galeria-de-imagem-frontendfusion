9f3625cb2a9d5d0ebb354e468ec51df31687ad73
Walassi Silva <walassi.b@gmail.com>
2024-10-19 14:43:47 -0300

    chore: config shadcn-ui

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
