c4904f20c40e2b8512a1362fbf22d7e46d12ceb2
Walassi Silva <walassi.b@gmail.com>
2024-10-20 15:43:06 -0300

    feat: add root link to `logo` component

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,6 +2,8 @@ import logo from "../../assets/logo.png";
 
 export default function Logo() {
   return (
-    <img src={logo} alt="logo" className="rounded-full border-2 h-20 w-20" />
+    <a href="/">
+      <img src={logo} alt="logo" className="rounded-full border-2 h-20 w-20 hover:rotate-12 duration-300" />
+    </a>
   );
 }
