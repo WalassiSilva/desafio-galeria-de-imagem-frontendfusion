3ee09fea0cd686e7a3d667fb9ab0389dc6edc66a
Walassi Silva <walassi.b@gmail.com>
2024-10-20 15:40:37 -0300

    fix: remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import FilterContextProvider from "./contexts/filter-context.jsx";
+import LikeContextProvider from "./contexts/like-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <LikeContextProvider>
     <FilterContextProvider>
       <App />
     </FilterContextProvider>
-  </React.StrictMode>
+  </LikeContextProvider>
 );
